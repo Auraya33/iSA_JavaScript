b49a267a618230ac18d2e47f896b281913c7f85b
auraya33 <kasia.nagel@gmail.com>
2017-12-11 10:14:01 +0100

    Initials now are always uppercase

--- getInitials.js
+++ getInitials.js
@@ -1,9 +1,9 @@
 // 2# Napisz funkcję, która przyjmuje dwa parametry - firstName oraz lastName, a następnie zwraca inicjały rozdzielone kropką, zwrócony string powinien składać się z wielkich liter.
 
-var  firstName = 'Kasia',
-     lastName = 'Nagel';
+var  firstName = 'kasia',
+     lastName = 'nagel';
 
 function GetInitials(firstName, lastName) {
-    return  firstName.slice(0,1) + '.' + lastName.slice(0, 1);
+    return  firstName.slice(0,1).toUpperCase() + '.' + lastName.slice(0, 1).toUpperCase();
 }
 console.log(GetInitials(firstName, lastName));
